2806984b3beafbed9a10fd455715a950344dd461
Rafael Laste <rafinhalaste@hotmail.com>
2025-05-12 10:29:43 -0300

    Alterações data e quantidade

--- src/pages/Compras/Index.js
+++ src/pages/Compras/Index.js
@@ -396,7 +396,7 @@ function Compras() {
                                                     timeFormat="HH:mm"
                                                     timeIntervals={1}
                                                     timeCaption="Hora"
-                                                    className="form__input"
+                                                    className="p-2 form__input"
                                                     placeholderText="Data e Hora* __/__/____  hh:mm"
                                                     maxDate={new Date()}
                                                 />
